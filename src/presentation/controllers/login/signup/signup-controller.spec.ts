36993b04482de62b2313f5b8a6b0aab6fd023d32
luan.lazzari <luanlazzari@hotmail.com>
2020-07-22 01:12:48 -0300

    test: ensure SignUpController returns 500 is AddAccount throws

--- src/presentation/controllers/login/signup/signup-controller.spec.ts
+++ src/presentation/controllers/login/signup/signup-controller.spec.ts
@@ -2,6 +2,32 @@ import { SignUpController } from './signup-controller'
 import { HttpRequest } from '@/presentation/protocols/http'
 import { AddAccount, AddAccountParams } from '@/domain/usecases/account/add-account'
 import { AccountModel } from '@/domain/models/account'
+import { throwError } from '@/domain/test/test-helper'
+import { ServerError } from '@/presentation/errors'
+import { serverError } from '@/presentation/helpers/http/http-helper'
+
+const mockAddAccount = (): AddAccount => {
+  class AddAccountStub implements AddAccount {
+    async add (account: AddAccountParams): Promise<AccountModel> {
+      return await Promise.resolve(mockAccountModel())
+    }
+  }
+  return new AddAccountStub()
+}
+
+type SutTypes = {
+  sut: SignUpController
+  addAccountStub: AddAccount
+}
+
+const makeSut = (): SutTypes => {
+  const addAccountStub = mockAddAccount()
+  const sut = new SignUpController(addAccountStub)
+  return {
+    sut,
+    addAccountStub
+  }
+}
 
 const mockAccountModel = (): AccountModel => ({
   id: 'any_id',
@@ -21,19 +47,20 @@ const mockRequest = (): HttpRequest => ({
 
 describe('SignUp Controller', () => {
   test('should call AddAccount with correct values', async () => {
-    class AddAccountStub implements AddAccount {
-      async add (account: AddAccountParams): Promise<AccountModel> {
-        return await Promise.resolve(mockAccountModel())
-      }
-    }
-    const addAccountStub = new AddAccountStub()
+    const { sut, addAccountStub } = makeSut()
     const addSpy = jest.spyOn(addAccountStub, 'add')
-    const sut = new SignUpController(addAccountStub)
     await sut.handle(mockRequest())
     expect(addSpy).toHaveBeenCalledWith({
       name: 'any_name',
       email: 'any_email',
       password: 'any_password'
     })
+  })
+
+  test('should return 500 if AddAccount throws', async () => {
+    const { sut, addAccountStub } = makeSut()
+    jest.spyOn(addAccountStub, 'add').mockImplementationOnce(throwError)
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(serverError(new ServerError(null)))
   })
 })
