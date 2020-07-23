326327dba72bde4e98cf629888b19c44f7f6d748
luan.lazzari <luanlazzari@hotmail.com>
2020-07-22 21:35:03 -0300

    feat: make validation composite

---
+++ src/validatiors/validation-composite.ts
@@ -0,0 +1,14 @@
+import { Validation } from '@/presentation/protocols/validation'
+
+export class ValidationComposite implements Validation {
+  constructor (private readonly validators: Validation[]) {}
+
+  validate (input: any): Error {
+    for (const validation of this.validators) {
+      const error = validation.validate(input)
+      if (error) {
+        return error
+      }
+    }
+  }
+}
