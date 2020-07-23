9d1c42f3e4082ebf1816543129c101de5bc77018
luan.lazzari <luanlazzari@hotmail.com>
2020-07-22 23:45:24 -0300

    test: ensure EmailValidation not returns if email is valid

---
+++ src/validation/validators/email-validation/email-validation.spec.ts
@@ -0,0 +1,42 @@
+import { EmailValidation } from './email-validation'
+import { InvalidParamError } from '@/presentation/errors'
+import { EmailValidator } from '@/validation/protocols/email-validation'
+import faker from 'faker'
+
+const mockEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
+
+type SutTypes = {
+  sut: EmailValidation
+  emailValidatorStub: EmailValidator
+}
+
+const makeSut = (): SutTypes => {
+  const emailValidatorStub = mockEmailValidator()
+  const sut = new EmailValidation(emailValidatorStub, 'email')
+  return {
+    sut,
+    emailValidatorStub
+  }
+}
+
+describe('EmailValidation Validation', () => {
+  test('Should return a InvalidParamError if validation fails', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const error = sut.validate({ email: 'invalid_email' })
+    expect(error).toEqual(new InvalidParamError('email'))
+  })
+
+  test('Should not return if validation success', () => {
+    const { sut } = makeSut()
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
+  })
+})
