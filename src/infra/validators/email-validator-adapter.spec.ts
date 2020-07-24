9cf2747c03ddc3f80e090fd56192e245c6be61f5
luan.lazzari <luanlazzari@hotmail.com>
2020-07-24 00:56:48 -0300

    test: ensure EmailValidatorAdapter returns false if email is invalid

--- src/infra/validators/email-validator-adapter.spec.ts
+++ src/infra/validators/email-validator-adapter.spec.ts
@@ -1,11 +1,18 @@
-import { EmailValidator } from './email-validator-adapter'
+import { EmailValidatorAdapter } from './email-validator-adapter'
 import validator from 'validator'
 
-describe('EmailValidator adapter', () => {
+describe('EmailValidatorAdapter adapter', () => {
   test('Should calls validator with correct email', () => {
-    const sut = new EmailValidator('email')
+    const sut = new EmailValidatorAdapter()
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
-    sut.validate({ email: 'any_email@mail.com' })
+    sut.isValid('any_email@mail.com')
     expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+
+  test('Should return false if email is invalid', () => {
+    const sut = new EmailValidatorAdapter()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+    const isValid = sut.isValid('any_email@mail.com')
+    expect(isValid).toBeFalsy()
   })
 })
