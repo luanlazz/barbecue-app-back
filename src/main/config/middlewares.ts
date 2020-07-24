0a29d147c5aebccb1990c18ea8b4dcff48b6ef0c
luan.lazzari <luanlazzari@hotmail.com>
2020-07-24 18:41:05 -0300

    feat: ensure cors is enable

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { bodyParser } from '@/main/middlewares/body-parser/body-parser'
+import { cors } from '@/main/middlewares/cors/cors'
 import { Express } from 'express'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
---
+++ src/main/middlewares/cors/cors.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+
+describe('CORS Middleware', () => {
+  test('Should enable CORS', async () => {
+    app.get('/test-cors', (req, res) => {
+      res.send()
+    })
+    await request(app)
+      .post('/test-cors')
+      .expect('access-control-allow-origin', '*')
+      .expect('access-control-allow-methods', '*')
+      .expect('access-control-allow-headers', '*')
+  })
+})
