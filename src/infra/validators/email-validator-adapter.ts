ea0a8a35f2eb9942d1110bcc8e92bb73cd3e3d56
luan.lazzari <luanlazzari@hotmail.com>
2020-07-24 00:56:19 -0300

    feat: ensure EmailValidatorAdapter returns false if email is invalid

--- src/infra/validators/email-validator-adapter.ts
+++ src/infra/validators/email-validator-adapter.ts
@@ -1,12 +1,9 @@
-import { Validation } from '@/presentation/protocols/validation'
-import { InvalidParamError } from '@/presentation/errors'
+import { EmailValidator } from '@/validation/protocols/email-validation'
 import validator from 'validator'
 
-export class EmailValidator implements Validation {
-  constructor (private readonly field: string) {}
-
-  validate (input: any): Error {
-    validator.isEmail(input[this.field])
-    return new InvalidParamError('email')
+export class EmailValidatorAdapter implements EmailValidator {
+  isValid (email: string): boolean {
+    validator.isEmail(email)
+    return false
   }
 }
