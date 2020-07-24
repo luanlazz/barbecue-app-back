02bc7bdce33e8d3195f20f755a467401537a111c
luan.lazzari <luanlazzari@hotmail.com>
2020-07-24 00:34:32 -0300

    feat: ensure sign return a token on success

--- src/infra/cryptoghaphy/jwt-adapter/jwt-adapter.ts
+++ src/infra/cryptoghaphy/jwt-adapter/jwt-adapter.ts
@@ -5,7 +5,7 @@ export class JwtAdapter implements Encrypter {
   constructor (private readonly secret: string) {}
 
   async encrypt (value: string): Promise<string> {
-    await jwt.sign({ id: value }, this.secret)
-    return Promise.resolve(null)
+    const token = await jwt.sign({ id: value }, this.secret)
+    return token
   }
 }
