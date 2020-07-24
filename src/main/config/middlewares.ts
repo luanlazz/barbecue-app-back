6e477286dce1f7cfd5bf23bd9ae61bc921506056
luan.lazzari <luanlazzari@hotmail.com>
2020-07-24 18:55:00 -0300

    feat: ensure requests return json as default type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { bodyParser } from '@/main/middlewares/body-parser/body-parser'
 import { cors } from '@/main/middlewares/cors/cors'
+import { contentType } from '@/main/middlewares/content-type/content-type'
 import { Express } from 'express'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
