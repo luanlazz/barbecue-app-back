ac67d289af4d3121008d310d2360600b8d28f5fb
luan.lazzari <luanlazzari@hotmail.com>
2020-07-24 00:17:55 -0300

    test: ensure JwtAdapter throw if sign throw

--- src/infra/cryptoghaphy/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptoghaphy/jwt-adapter/jwt-adapter.spec.ts
@@ -1,11 +1,19 @@
 import { JwtAdapter } from './jwt-adapter'
+import { throwError } from '@/domain/test'
 import jwt from 'jsonwebtoken'
 
 describe('Jwt adapter', () => {
-  test('Should call encrypt with correct value', async () => {
+  test('Should call sign with correct value', async () => {
     const sut = new JwtAdapter('secret')
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encrypt('any_value')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_value' }, 'secret')
+  })
+
+  test('Should throw if sign throws', async () => {
+    const sut = new JwtAdapter('secret')
+    jest.spyOn(jwt, 'sign').mockImplementation(throwError)
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
   })
 })
