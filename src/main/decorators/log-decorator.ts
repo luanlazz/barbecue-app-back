0b86f09cc1f8fc0dd4fe0f3dcb60e4e0cd0b86de
luan.lazzari <luanlazzari@hotmail.com>
2020-07-25 11:41:16 -0300

    feat: ensure LogControllerDecorator calls LogErrorRepository with correct stack

--- src/main/decorators/log-decorator.ts
+++ src/main/decorators/log-decorator.ts
@@ -1,11 +1,18 @@
 import { Controller } from '@/presentation/protocols/controller'
 import { HttpResponse, HttpRequest } from '@/presentation/protocols/http'
+import { LogErrorRepository } from '@/data/protocols/log/log-error-repository'
 
 export class LogControllerDecorator implements Controller {
-  constructor (private readonly controller: Controller) {}
+  constructor (
+    private readonly controller: Controller,
+    private readonly logErrorRepository: LogErrorRepository
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest)
+    if (httpResponse.statusCode === 500) {
+      await this.logErrorRepository.log(httpResponse.body.stack)
+    }
     return httpResponse
   }
 }
