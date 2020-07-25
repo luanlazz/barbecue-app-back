e8ab58cd186d991e863bfe9917fe722b2d2f26b6
luan.lazzari <luanlazzari@hotmail.com>
2020-07-25 18:50:25 -0300

    test: ensure LoginController return 400 if validate return an error

--- src/presentation/controllers/login/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login/login-controller.spec.ts
@@ -2,6 +2,8 @@ import { LoginController } from './login-controller'
 import { HttpRequest } from '@/presentation/protocols/http'
 import { mockValidation } from '@/presentation/test'
 import { Validation } from '@/presentation/protocols/validation'
+import { MissingParamError } from '@/presentation/errors'
+import { badRequest } from '@/presentation/helpers/http/http-helper'
 
 type SutTypes = {
   sut: LoginController
@@ -33,5 +35,12 @@ describe('SignUp Controller', () => {
       email: 'any_email@mail.com',
       password: 'any_password'
     })
+  })
+
+  test('should return 400 if validate return an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('email'))
+    const error = await sut.handle(mockRequest())
+    expect(error).toEqual(badRequest(new MissingParamError('email')))
   })
 })
