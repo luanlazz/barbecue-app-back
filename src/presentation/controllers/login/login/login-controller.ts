2233860312131f0e1b0e4c61f4e1b346dbf76bb2
luan.lazzari <luanlazzari@hotmail.com>
2020-07-25 19:39:13 -0300

    feat: ensure LoginController calls Authentication with correct values

--- src/presentation/controllers/login/login/login-controller.ts
+++ src/presentation/controllers/login/login/login-controller.ts
@@ -2,10 +2,12 @@ import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
 import { Controller } from '@/presentation/protocols/controller'
 import { Validation } from '@/presentation/protocols/validation'
 import { badRequest, serverError } from '@/presentation/helpers/http/http-helper'
+import { Authentication } from '@/domain/usecases/account/authentication'
 
 export class LoginController implements Controller {
   constructor (
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly authentication: Authentication
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -14,6 +16,12 @@ export class LoginController implements Controller {
       if (error) {
         return badRequest(error)
       }
+
+      await this.authentication.auth({
+        email: httpRequest.body.email,
+        password: httpRequest.body.password
+      })
+
       return Promise.resolve(null)
     } catch (error) {
       return serverError(error)
