6da5146a467ba0fb8834d95783843a7812d5df24
luan.lazzari <luanlazzari@hotmail.com>
2020-07-25 18:49:56 -0300

    feat: ensure LoginController return 400 if validate return an error

--- src/presentation/controllers/login/login/login-controller.ts
+++ src/presentation/controllers/login/login/login-controller.ts
@@ -1,6 +1,7 @@
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
 import { Controller } from '@/presentation/protocols/controller'
 import { Validation } from '@/presentation/protocols/validation'
+import { badRequest } from '@/presentation/helpers/http/http-helper'
 
 export class LoginController implements Controller {
   constructor (
@@ -8,7 +9,10 @@ export class LoginController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body)
+    const error = this.validation.validate(httpRequest.body)
+    if (error) {
+      return badRequest(error)
+    }
     return Promise.resolve(null)
   }
 }
