24d5e79a06709653d4f552fbaf6786a2f75facde
luan.lazzari <luanlazzari@hotmail.com>
2020-07-26 01:02:51 -0300

    test: ensure DbLoadAccountByToken throws if LoadAccountByToken throws

--- src/data/usecases/account/load-account-by-token/load-account-by-token.spec.ts
+++ src/data/usecases/account/load-account-by-token/load-account-by-token.spec.ts
@@ -1,6 +1,7 @@
 import { DbLoadAccountByToken } from './load-account-by-token'
 import { LoadAccountByToken } from '@/domain/usecases/account/load-account-by-token'
 import { mockLoadAccountByTokenRepository } from '@/data/test'
+import { throwError } from '@/domain/test'
 
 type SutTypes = {
   sut: DbLoadAccountByToken
@@ -22,5 +23,12 @@ describe('Authentication use case', () => {
     const loadByTokenSpy = jest.spyOn(loadAccountByTokenStub, 'loadByToken')
     await sut.loadByToken('any_token')
     expect(loadByTokenSpy).toHaveBeenCalledWith('any_token')
+  })
+
+  test('Should throws if LoadAccountByEmailRepository throws', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    jest.spyOn(loadAccountByTokenStub, 'loadByToken').mockImplementation(throwError)
+    const error = sut.loadByToken('any_token')
+    await expect(error).rejects.toThrow()
   })
 })
