715690ce13f7790059b0cd785c93f7ab106c20a2
luan.lazzari <luanlazzari@hotmail.com>
2020-07-26 16:27:33 -0300

    feat: ensure load barbecue route return 403 without access token

--- src/main/routes/barbecue-routes.ts
+++ src/main/routes/barbecue-routes.ts
@@ -1,8 +1,10 @@
 import { makeSaveBarbecueController } from '@/main/factories/controllers/barbecue/save/save-barbecue-controller-factory'
+import { makeLoadBarbecuesController } from '@/main/factories/controllers/barbecue/load-all/load-barbecues-controller-factory'
 import { adaptRoute } from '@/main/adapters/express-route-adapter'
 import { auth } from '@/main/middlewares/auth/auth'
 import { Router } from 'express'
 
 export default (route: Router): void => {
   route.put('/barbecue/:barbecueId?', auth, adaptRoute(makeSaveBarbecueController()))
+  route.get('/barbecue', auth, adaptRoute(makeLoadBarbecuesController()))
 }
