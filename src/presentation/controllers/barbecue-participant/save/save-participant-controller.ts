ce875db0583ffb43d9212db89e114988a758117b
luan.lazzari <luanlazzari@hotmail.com>
2020-07-26 19:49:49 -0300

    feat: ensure SaveParticipantController calls Validation with correct values

---
+++ src/presentation/controllers/barbecue-participant/save/save-participant-controller.ts
@@ -0,0 +1,14 @@
+import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
+import { Controller } from '@/presentation/protocols/controller'
+import { Validation } from '@/presentation/protocols/validation'
+
+export class SaveParticipantController implements Controller {
+  constructor (
+    private readonly validation: Validation
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    this.validation.validate(httpRequest.body)
+    return Promise.resolve(null)
+  }
+}
