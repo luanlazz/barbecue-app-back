76fc5051f587db890199888cda88363ebbed3bbd
luan.lazzari <luanlazzari@hotmail.com>
2020-07-26 13:59:30 -0300

    fix: ensure barbecue generate a new ObjectId if not provide

--- src/infra/db/mongodb/barbecue-repository/barbecue.ts
+++ src/infra/db/mongodb/barbecue-repository/barbecue.ts
@@ -2,10 +2,13 @@ import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import { SaveBarbecueRepository } from '@/data/protocols/db/barbecue/save-barbecue-repository'
 import { barbecueParams } from '@/domain/usecases/barbecue/save-barbecue'
 import { BarbecueModel } from '@/domain/models/barbecue'
+import { ObjectId } from 'mongodb'
 
 export class BarbecueMongoRepository implements SaveBarbecueRepository {
   async save (barbecue: barbecueParams): Promise<BarbecueModel> {
     const barbecueCollection = await MongoHelper.getCollection('barbecues')
+
+    if (!barbecue.barbecueId) barbecue.barbecueId = new ObjectId().toHexString()
 
     const result = await barbecueCollection.findOneAndUpdate({
       _id: barbecue.barbecueId,
