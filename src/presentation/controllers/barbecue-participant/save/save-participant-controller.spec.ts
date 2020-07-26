06f8a64dd19b7cb837d4c34491a0b3b043131fb6
luan.lazzari <luanlazzari@hotmail.com>
2020-07-26 19:57:57 -0300

    test: ensure SaveParticipantController return bad request if Validation fails

--- src/presentation/controllers/barbecue-participant/save/save-participant-controller.spec.ts
+++ src/presentation/controllers/barbecue-participant/save/save-participant-controller.spec.ts
@@ -2,6 +2,7 @@ import { SaveParticipantController } from './save-participant-controller'
 import { HttpRequest } from '@/presentation/protocols/http'
 import { Validation } from '@/presentation/protocols/validation'
 import { mockValidation } from '@/presentation/test'
+import { badRequest } from '@/presentation/helpers/http/http-helper'
 
 type SutTypes = {
   sut: SaveParticipantController
@@ -36,5 +37,12 @@ describe('SaveBarbecue Controller', () => {
     const validateSpy = jest.spyOn(validationStub, 'validate')
     await sut.handle(mockRequest())
     expect(validateSpy).toHaveBeenCalledWith(mockRequest().body)
+  })
+
+  test('should return 400 if Validation fails', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(badRequest(new Error()))
   })
 })
