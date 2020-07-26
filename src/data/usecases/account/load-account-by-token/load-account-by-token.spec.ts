318cb1d0c060d3459f825121f0aace9f509c9b03
luan.lazzari <luanlazzari@hotmail.com>
2020-07-26 01:11:15 -0300

    test: ensure DbLoadAccountByToken calls Decrypter with correct token

--- src/data/usecases/account/load-account-by-token/load-account-by-token.spec.ts
+++ src/data/usecases/account/load-account-by-token/load-account-by-token.spec.ts
@@ -1,23 +1,34 @@
 import { DbLoadAccountByToken } from './load-account-by-token'
+import { Decrypter } from '@/data/protocols/cryptography/decrypter'
+import { mockLoadAccountByTokenRepository, mockDecrypter } from '@/data/test'
 import { LoadAccountByToken } from '@/domain/usecases/account/load-account-by-token'
-import { mockLoadAccountByTokenRepository } from '@/data/test'
 import { throwError } from '@/domain/test'
 
 type SutTypes = {
   sut: DbLoadAccountByToken
+  decrypterStub: Decrypter
   loadAccountByTokenStub: LoadAccountByToken
 }
 
 const makeSut = (): SutTypes => {
+  const decrypterStub = mockDecrypter()
   const loadAccountByTokenStub = mockLoadAccountByTokenRepository()
-  const sut = new DbLoadAccountByToken(loadAccountByTokenStub)
+  const sut = new DbLoadAccountByToken(decrypterStub, loadAccountByTokenStub)
   return {
     sut,
+    decrypterStub,
     loadAccountByTokenStub
   }
 }
 
 describe('Authentication use case', () => {
+  test('Should call Decrypter with correct email', async () => {
+    const { sut, decrypterStub } = makeSut()
+    const decryptSpy = jest.spyOn(decrypterStub, 'decrypt')
+    await sut.loadByToken('any_token')
+    expect(decryptSpy).toHaveBeenCalledWith('any_token')
+  })
+
   test('Should call LoadAccountByEmailRepository with correct email', async () => {
     const { sut, loadAccountByTokenStub } = makeSut()
     const loadByTokenSpy = jest.spyOn(loadAccountByTokenStub, 'loadByToken')
