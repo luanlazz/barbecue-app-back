f9777d9358795b000cebba609b889bce251e81a1
luan.lazzari <luanlazzari@hotmail.com>
2020-07-26 00:29:22 -0300

    feat: ensure AuthMiddleware throws if LoadAccountByToken throws

--- src/presentation/middleware/auth-middleware.ts
+++ src/presentation/middleware/auth-middleware.ts
@@ -1,6 +1,6 @@
 import { Middleware } from '@/presentation/protocols/middleware'
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
-import { forbidden } from '@/presentation/helpers/http/http-helper'
+import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
 import { AccessDeniedError } from '@/presentation/errors'
 import { LoadAccountByToken } from '@/domain/usecases/account/load-account-by-token'
 
@@ -8,10 +8,14 @@ export class AuthMiddleware implements Middleware {
   constructor (private readonly loadAccountByTokenStub: LoadAccountByToken) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const accessToken = httpRequest.headers?.['x-access-token']
-    if (accessToken) {
-      await this.loadAccountByTokenStub.loadByToken(accessToken)
+    try {
+      const accessToken = httpRequest.headers?.['x-access-token']
+      if (accessToken) {
+        await this.loadAccountByTokenStub.loadByToken(accessToken)
+      }
+      return forbidden(new AccessDeniedError())
+    } catch (error) {
+      return serverError(error)
     }
-    return Promise.resolve(forbidden(new AccessDeniedError()))
   }
 }
