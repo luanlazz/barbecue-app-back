8aa5c4d5728d6ec03807977f73b1d5d2e7634562
luan.lazzari <luanlazzari@hotmail.com>
2020-07-26 00:37:24 -0300

    feat: ensure AuthMiddleware return account id on success

--- src/presentation/middleware/auth-middleware.ts
+++ src/presentation/middleware/auth-middleware.ts
@@ -1,6 +1,6 @@
 import { Middleware } from '@/presentation/protocols/middleware'
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
-import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
+import { forbidden, serverError, ok } from '@/presentation/helpers/http/http-helper'
 import { AccessDeniedError } from '@/presentation/errors'
 import { LoadAccountByToken } from '@/domain/usecases/account/load-account-by-token'
 
@@ -10,9 +10,14 @@ export class AuthMiddleware implements Middleware {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const accessToken = httpRequest.headers?.['x-access-token']
+
       if (accessToken) {
-        await this.loadAccountByTokenStub.loadByToken(accessToken)
+        const account = await this.loadAccountByTokenStub.loadByToken(accessToken)
+        if (account) {
+          return ok({ accountId: account.id })
+        }
       }
+
       return forbidden(new AccessDeniedError())
     } catch (error) {
       return serverError(error)
