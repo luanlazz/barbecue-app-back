453378d77306a4e61d23f1aa8b858ceb2efa562f
luan.lazzari <luanlazzari@hotmail.com>
2020-07-26 15:51:49 -0300

    test: ensure DbLoadBarbecue throws if LoadBarbecuesRepository throws

--- src/data/usecases/barbecue/load-barbecues/load-barbecue.spec.ts
+++ src/data/usecases/barbecue/load-barbecues/load-barbecue.spec.ts
@@ -1,6 +1,7 @@
 import { DbLoadBarbecue } from './load-barbecue'
 import { mockLoadBarbecuesRepository } from '@/data/test'
 import { LoadBarbecuesRepository } from '@/data/protocols/db/barbecue/load-barbecue-repository'
+import { throwError } from '@/domain/test'
 
 type SutTypes = {
   sut: DbLoadBarbecue
@@ -16,11 +17,18 @@ const makeSut = (): SutTypes => {
   }
 }
 
-describe('SaveBarbecue use case', () => {
-  test('Should call SaveBarbecue with correct values', async () => {
+describe('LoadBarbecue use case', () => {
+  test('Should call LoadBarbecue with correct values', async () => {
     const { sut, loadBarbecuesRepositoryStub } = makeSut()
     const hashSpy = jest.spyOn(loadBarbecuesRepositoryStub, 'loadAll')
     await sut.load('any_account_id')
     expect(hashSpy).toHaveBeenCalledWith('any_account_id')
+  })
+
+  test('Should throws if LoadBarbecue throws', async () => {
+    const { sut, loadBarbecuesRepositoryStub } = makeSut()
+    jest.spyOn(loadBarbecuesRepositoryStub, 'loadAll').mockImplementation(throwError)
+    const barbecue = sut.load('any_account_id')
+    await expect(barbecue).rejects.toThrow()
   })
 })
