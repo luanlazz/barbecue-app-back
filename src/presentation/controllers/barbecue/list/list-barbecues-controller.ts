91d9a69d93a1484b87a82821680980dadfc4cf9d
luan.lazzari <luanlazzari@hotmail.com>
2020-07-26 15:17:49 -0300

    feat: ensure LoadBarbecuesController throws if LoadBarbecues throws

--- src/presentation/controllers/barbecue/list/list-barbecues-controller.ts
+++ src/presentation/controllers/barbecue/list/list-barbecues-controller.ts
@@ -1,12 +1,17 @@
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
 import { Controller } from '@/presentation/protocols/controller'
+import { serverError } from '@/presentation/helpers/http/http-helper'
 import { LoadBarbecues } from '@/domain/usecases/barbecue/list-barbecues'
 
 export class LoadBarbecuesController implements Controller {
   constructor (private readonly loadBarbecues: LoadBarbecues) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadBarbecues.load(httpRequest.accountId)
-    return null
+    try {
+      await this.loadBarbecues.load(httpRequest.accountId)
+      return null
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
