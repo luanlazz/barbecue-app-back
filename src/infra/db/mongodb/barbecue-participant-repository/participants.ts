9895358c47e47404d3a58a966ce0975215944a21
luan.lazzari <luanlazzari@hotmail.com>
2020-07-27 19:16:48 -0300

    feat: ensure ParticipantsMongoRepository retuan a list of participants

--- src/infra/db/mongodb/barbecue-participant-repository/participants.ts
+++ src/infra/db/mongodb/barbecue-participant-repository/participants.ts
@@ -4,7 +4,6 @@ import { LoadParticipantsByBqRepository } from '@/data/protocols/db/barbecue-par
 import { SaveParticipantParams } from '@/domain/usecases/barbecue-participant/save-barbecue-participant'
 import { ObjectId } from 'mongodb'
 import { ParticipantModel } from '@/domain/models/participant'
-import { QueryBuilder } from '../helpers/query-builder'
 
 export class ParticipantsMongoRepository implements SaveParticipantRepository,
                                                     LoadParticipantsByBqRepository {
@@ -32,48 +31,9 @@ export class ParticipantsMongoRepository implements SaveParticipantRepository,
 
   async load (barbecueId: string): Promise<ParticipantModel[]> {
     const participantCollection = await MongoHelper.getCollection('participants')
-    const participantsRes = await participantCollection.find({
+    const participants = await participantCollection.find({
       barbecueId: new ObjectId(barbecueId)
     }).toArray()
-
-    const { valueTotalDrink, valueTotalFood } = await this.getTotal(barbecueId)
-    const countDrink = participantsRes.filter(participant => participant.drink)
-    const countFood = participantsRes.filter(participant => participant.food)
-
-    const drinkDivide = valueTotalDrink / countDrink.length
-    const foodDivide = valueTotalFood / countFood.length
-
-    const participantsCalculate = participantsRes.map(participant => (
-      {
-        ...participant,
-        value: this.calculateContribution(drinkDivide, foodDivide, participant.drink, participant.food)
-      }
-    ))
-
-    return participantsCalculate
-  }
-
-  calculateContribution (valueDrink: number, valueFood: number, drink: boolean, food: boolean): number {
-    const drinkTotal = drink ? valueDrink : 0
-    const foodTotal = food ? valueFood : 0
-    return drinkTotal + foodTotal
-  }
-
-  async getTotal (barbecueId: string): Promise<any> {
-    const barbecuesCollection = await MongoHelper.getCollection('barbecues')
-
-    const query = new QueryBuilder()
-      .match({
-        _id: new ObjectId(barbecueId)
-      })
-      .project({
-        valueTotalDrink: 1,
-        valueTotalFood: 1,
-        _id: 0
-      })
-      .build()
-
-    const surveyResult = await barbecuesCollection.aggregate(query).toArray()
-    return surveyResult[0]
+    return MongoHelper.mapCollection(participants)
   }
 }
