830cc3cd814b0d4a7c7b3dae8a9b93aff41100a3
luan.lazzari <luanlazzari@hotmail.com>
2020-07-26 21:30:18 -0300

    refactor: change interface return

--- src/data/protocols/db/barbecue-participant/db-save-participant.ts
+++ src/data/protocols/db/barbecue-participant/db-save-participant.ts
@@ -1,6 +1,5 @@
 import { SaveParticipantParams } from '@/domain/usecases/barbecue-participant/save-barbecue-participant'
-import { ParticipantModel } from '@/domain/models/participant'
 
 export interface SaveParticipantRepository {
-  save(participant: SaveParticipantParams): Promise<ParticipantModel[]>
+  save(participant: SaveParticipantParams): Promise<void>
 }
