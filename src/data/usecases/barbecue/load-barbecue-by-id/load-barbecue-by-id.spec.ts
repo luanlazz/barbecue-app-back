9c77ce9f5a08b91bbd853707096e7712711de80d
luan.lazzari <luanlazzari@hotmail.com>
2020-07-27 19:42:57 -0300

    test: ensure DbLoadBarbecueById return a barbecue on success

--- src/data/usecases/barbecue/load-barbecue-by-id/load-barbecue-by-id.spec.ts
+++ src/data/usecases/barbecue/load-barbecue-by-id/load-barbecue-by-id.spec.ts
@@ -1,6 +1,7 @@
 import { DbLoadBarbecueById } from './load-barbecue-by-id'
 import { mockLoadBarbecueByIdRepository } from '@/data/test'
 import { LoadBarbecueByIdRepository } from '@/data/protocols/db/barbecue/load-barbecue-by-id-repository'
+import { throwError, mockBarbecueModel } from '@/domain/test'
 
 type SutTypes = {
   sut: DbLoadBarbecueById
@@ -20,7 +21,20 @@ describe('LoadBarbecueById use case', () => {
   test('Should call LoadBarbecueById with correct values', async () => {
     const { sut, loadBarbecueByIdRepositoryStub } = makeSut()
     const hashSpy = jest.spyOn(loadBarbecueByIdRepositoryStub, 'loadById')
-    await sut.loadById('any_account_id')
-    expect(hashSpy).toHaveBeenCalledWith('any_account_id')
+    await sut.loadById('any_barbecue_id')
+    expect(hashSpy).toHaveBeenCalledWith('any_barbecue_id')
+  })
+
+  test('Should throws if LoadBarbecueById throws', async () => {
+    const { sut, loadBarbecueByIdRepositoryStub } = makeSut()
+    jest.spyOn(loadBarbecueByIdRepositoryStub, 'loadById').mockImplementation(throwError)
+    const barbecue = sut.loadById('any_barbecue_id')
+    await expect(barbecue).rejects.toThrow()
+  })
+
+  test('Should a barbecue on success', async () => {
+    const { sut } = makeSut()
+    const barbecue = await sut.loadById('any_barbecue_id')
+    expect(barbecue).toEqual(mockBarbecueModel())
   })
 })
