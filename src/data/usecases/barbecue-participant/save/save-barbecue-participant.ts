6ebf79c535c571ea15303fd451058cc34c80a43d
luan.lazzari <luanlazzari@hotmail.com>
2020-07-26 21:26:05 -0300

    feat: ensure DbSaveParticipant calls LoadParticipantsByBqRepository with correct values

--- src/data/usecases/barbecue-participant/save/save-barbecue-participant.ts
+++ src/data/usecases/barbecue-participant/save/save-barbecue-participant.ts
@@ -1,12 +1,17 @@
 import { SaveParticipant, SaveParticipantParams } from '@/domain/usecases/barbecue-participant/save-barbecue-participant'
 import { SaveParticipantRepository } from '@/data/protocols/db/barbecue-participant/db-save-participant'
+import { LoadParticipantsByBqRepository } from '@/data/protocols/db/barbecue-participant/db-load-participants-by-bq'
 import { ParticipantModel } from '@/domain/models/participant'
 
 export class DbSaveParticipant implements SaveParticipant {
-  constructor (private readonly saveParticipantRepository: SaveParticipantRepository) {}
+  constructor (
+    private readonly saveParticipantRepository: SaveParticipantRepository,
+    private readonly loadParticipantsByBqRepository: LoadParticipantsByBqRepository
+  ) {}
 
   async save (participant: SaveParticipantParams): Promise<ParticipantModel[]> {
-    const participants = await this.saveParticipantRepository.save(participant)
+    await this.saveParticipantRepository.save(participant)
+    const participants = await this.loadParticipantsByBqRepository.load(participant.barbecueId)
     return participants
   }
 }
