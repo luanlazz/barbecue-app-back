15acfff799f5106417a76d69adc7cc7872ec7c55
luan.lazzari <luanlazzari@hotmail.com>
2020-07-27 21:43:06 -0300

    feat: ensure CalculateContribution return a list of participants on success

--- src/data/usecases/barbecue-participant/calculate/calculate-contribution.ts
+++ src/data/usecases/barbecue-participant/calculate/calculate-contribution.ts
@@ -4,6 +4,25 @@ import { BarbecueModel } from '@/domain/models/barbecue'
 
 export class CalculateContribution implements calculateContribution {
   calculate (barbecue: BarbecueModel, participants: ParticipantModel[]): ParticipantModel[] {
-    return null
+    const countDrink = participants.filter(participant => participant.drink)
+    const countFood = participants.filter(participant => participant.food)
+
+    const drinkDivide = barbecue.valueTotalDrink / countDrink.length
+    const foodDivide = barbecue.valueTotalFood / countFood.length
+
+    const participantsCalculate = participants.map(participant => (
+      {
+        ...participant,
+        value: this.calculateContribution(drinkDivide, foodDivide, participant.drink, participant.food)
+      }
+    ))
+
+    return participantsCalculate
+  }
+
+  calculateContribution (valueDrink: number, valueFood: number, drink: boolean, food: boolean): number {
+    const drinkTotal = drink ? valueDrink : 0
+    const foodTotal = food ? valueFood : 0
+    return drinkTotal + foodTotal
   }
 }
