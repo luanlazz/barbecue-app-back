a9a4af5b57edc22ca931423972ae2ffe3afa1795
luan.lazzari <luanlazzari@hotmail.com>
2020-07-28 00:22:19 -0300

    feat: ensure SaveParticipantController call LoadParticipants with correct barbecue id

--- src/presentation/controllers/barbecue-participant/save/save-participant-controller.ts
+++ src/presentation/controllers/barbecue-participant/save/save-participant-controller.ts
@@ -3,15 +3,13 @@ import { Controller } from '@/presentation/protocols/controller'
 import { Validation } from '@/presentation/protocols/validation'
 import { badRequest, serverError, ok } from '@/presentation/helpers/http/http-helper'
 import { SaveParticipant } from '@/domain/usecases/barbecue-participant/save-participant'
-import { LoadBarbecueById } from '@/domain/usecases/barbecue/load-barbecue-by-id'
-import { CalculateContribution } from '@/domain/usecases/barbecue-participant/calculate-contribution'
+import { LoadParticipants } from '@/domain/usecases/barbecue-participant/load-participants'
 
 export class SaveParticipantController implements Controller {
   constructor (
     private readonly validation: Validation,
     private readonly saveParticipant: SaveParticipant,
-    private readonly loadBarbecueById: LoadBarbecueById,
-    private readonly calculateContribution: CalculateContribution
+    private readonly loadParticipants: LoadParticipants
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -22,13 +20,11 @@ export class SaveParticipantController implements Controller {
       const { name, food, drink, pay } = httpRequest.body
       const { barbecueId, participantId } = httpRequest.params
 
-      const participants = await this.saveParticipant.save({ barbecueId, participantId, name, food, drink, pay })
+      await this.saveParticipant.save({ barbecueId, participantId, name, food, drink, pay })
 
-      const barbecue = await this.loadBarbecueById.loadById(barbecueId)
+      const participants = await this.loadParticipants.load(barbecueId)
 
-      const participantsContribution = this.calculateContribution.calculate(barbecue, participants)
-
-      return ok(participantsContribution)
+      return ok(participants)
     } catch (error) {
       return serverError(error)
     }
