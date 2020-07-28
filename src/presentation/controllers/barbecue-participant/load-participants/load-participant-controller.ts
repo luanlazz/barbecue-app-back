95c0f62ce2185057423924ecd39c43d14dff8811
luan.lazzari <luanlazzari@hotmail.com>
2020-07-28 00:10:18 -0300

    feat: ensure LoadParticipantsController return 204 if returns empty list

--- src/presentation/controllers/barbecue-participant/load-participants/load-participant-controller.ts
+++ src/presentation/controllers/barbecue-participant/load-participants/load-participant-controller.ts
@@ -1,15 +1,15 @@
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
 import { Controller } from '@/presentation/protocols/controller'
 import { LoadParticipants } from '@/domain/usecases/barbecue-participant/load-participants'
-import { serverError } from '@/presentation/helpers/http/http-helper'
+import { serverError, noContent, ok } from '@/presentation/helpers/http/http-helper'
 
 export class LoadParticipantsController implements Controller {
   constructor (private readonly loadParticipants: LoadParticipants) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      await this.loadParticipants.load(httpRequest.params.barbecueId)
-      return Promise.resolve(null)
+      const participants = await this.loadParticipants.load(httpRequest.params.barbecueId)
+      return participants.length ? ok(participants) : noContent()
     } catch (error) {
       return serverError(error)
     }
