b693d96f51e07af56cf5085e9b4bed4ff3f9c777
luan.lazzari <luanlazzari@hotmail.com>
2020-07-28 00:51:03 -0300

    feat: ensure DbLoadParticipants call CalculateContribution with correct values

--- src/data/usecases/barbecue-participant/load-participants/load-participants.ts
+++ src/data/usecases/barbecue-participant/load-participants/load-participants.ts
@@ -2,16 +2,19 @@ import { LoadParticipantsByBqRepository } from '@/data/protocols/db/barbecue-par
 import { LoadParticipants } from '@/domain/usecases/barbecue-participant/load-participants'
 import { ParticipantModel } from '@/domain/models/participant'
 import { LoadBarbecueByIdRepository } from '@/data/protocols/db/barbecue/load-barbecue-by-id-repository'
+import { CalculateContribution } from '@/domain/usecases/barbecue-participant/calculate-contribution'
 
 export class DbLoadParticipants implements LoadParticipants {
   constructor (
     private readonly loadParticipantsByBqRepository: LoadParticipantsByBqRepository,
-    private readonly loadBarbecueByIdRepository: LoadBarbecueByIdRepository
+    private readonly loadBarbecueByIdRepository: LoadBarbecueByIdRepository,
+    private readonly calculateContribution: CalculateContribution
   ) {}
 
   async load (barbecueId: string): Promise<ParticipantModel[]> {
     const participants = await this.loadParticipantsByBqRepository.load(barbecueId)
-    await this.loadBarbecueByIdRepository.loadById(barbecueId)
+    const barbecue = await this.loadBarbecueByIdRepository.loadById(barbecueId)
+    this.calculateContribution.calculate(barbecue, participants)
     return participants
   }
 }
