873cf985777642772cbac98c6f656385c812ad16
luan.lazzari <luanlazzari@hotmail.com>
2020-07-28 00:42:13 -0300

    feat: ensure DbLoadParticipants calls LoadBarbecueByIdRepository with correct id

--- src/data/usecases/barbecue-participant/load-participants/load-participants.ts
+++ src/data/usecases/barbecue-participant/load-participants/load-participants.ts
@@ -1,12 +1,17 @@
 import { LoadParticipantsByBqRepository } from '@/data/protocols/db/barbecue-participant/db-load-participants-by-bq'
 import { LoadParticipants } from '@/domain/usecases/barbecue-participant/load-participants'
 import { ParticipantModel } from '@/domain/models/participant'
+import { LoadBarbecueByIdRepository } from '@/data/protocols/db/barbecue/load-barbecue-by-id-repository'
 
 export class DbLoadParticipants implements LoadParticipants {
-  constructor (private readonly loadParticipantsByBqRepository: LoadParticipantsByBqRepository) {}
+  constructor (
+    private readonly loadParticipantsByBqRepository: LoadParticipantsByBqRepository,
+    private readonly loadBarbecueByIdRepository: LoadBarbecueByIdRepository
+  ) {}
 
   async load (barbecueId: string): Promise<ParticipantModel[]> {
     const participants = await this.loadParticipantsByBqRepository.load(barbecueId)
+    await this.loadBarbecueByIdRepository.loadById(barbecueId)
     return participants
   }
 }
