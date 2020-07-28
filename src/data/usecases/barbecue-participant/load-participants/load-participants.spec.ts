8b01d1f58e89e90974d74fda1f1f70b2c70958ea
luan.lazzari <luanlazzari@hotmail.com>
2020-07-28 00:42:50 -0300

    test: ensure DbLoadParticipants calls LoadBarbecueByIdRepository with correct id

--- src/data/usecases/barbecue-participant/load-participants/load-participants.spec.ts
+++ src/data/usecases/barbecue-participant/load-participants/load-participants.spec.ts
@@ -1,29 +1,33 @@
 import { DbLoadParticipants } from './load-participants'
-import { mockLoadParticipantByBqRepository } from '@/data/test'
+import { mockLoadParticipantByBqRepository, mockLoadBarbecueByIdRepository } from '@/data/test'
 import { LoadParticipantsByBqRepository } from '@/data/protocols/db/barbecue-participant/db-load-participants-by-bq'
 import { mockParticipantsModel } from '@/domain/test/mock-participant'
 import { throwError } from '@/domain/test'
+import { LoadBarbecueByIdRepository } from '@/data/protocols/db/barbecue/load-barbecue-by-id-repository'
 
 type SutTypes = {
   sut: DbLoadParticipants
   loadParticipantsByBqRepositoryStub: LoadParticipantsByBqRepository
+  loadBarbecueByIdRepositoryStub: LoadBarbecueByIdRepository
 }
 
 const makeSut = (): SutTypes => {
   const loadParticipantsByBqRepositoryStub = mockLoadParticipantByBqRepository()
-  const sut = new DbLoadParticipants(loadParticipantsByBqRepositoryStub)
+  const loadBarbecueByIdRepositoryStub = mockLoadBarbecueByIdRepository()
+  const sut = new DbLoadParticipants(loadParticipantsByBqRepositoryStub, loadBarbecueByIdRepositoryStub)
   return {
     sut,
-    loadParticipantsByBqRepositoryStub
+    loadParticipantsByBqRepositoryStub,
+    loadBarbecueByIdRepositoryStub
   }
 }
 
 describe('LoadParticipants use case', () => {
   test('Should call LoadParticipantsByBqRepository with correct values', async () => {
     const { sut, loadParticipantsByBqRepositoryStub } = makeSut()
-    const hashSpy = jest.spyOn(loadParticipantsByBqRepositoryStub, 'load')
+    const loadSpy = jest.spyOn(loadParticipantsByBqRepositoryStub, 'load')
     await sut.load('any_barbecue_id')
-    expect(hashSpy).toHaveBeenCalledWith('any_barbecue_id')
+    expect(loadSpy).toHaveBeenCalledWith('any_barbecue_id')
   })
 
   test('Should throws if LoadParticipantsByBqRepository throws', async () => {
@@ -31,6 +35,13 @@ describe('LoadParticipants use case', () => {
     jest.spyOn(loadParticipantsByBqRepositoryStub, 'load').mockImplementation(throwError)
     const participants = sut.load('any_barbecue_id')
     await expect(participants).rejects.toThrow()
+  })
+
+  test('Should call LoadBarbecueByIdRepository with correct values', async () => {
+    const { sut, loadBarbecueByIdRepositoryStub } = makeSut()
+    const loadByIdSpy = jest.spyOn(loadBarbecueByIdRepositoryStub, 'loadById')
+    await sut.load('any_barbecue_id')
+    expect(loadByIdSpy).toHaveBeenCalledWith('any_barbecue_id')
   })
 
   test('Should a list of participants on success', async () => {
