d8c379f49683fe29c9d684d7e5199ffd3e9c9a71
luan.lazzari <luanlazzari@hotmail.com>
2020-07-28 00:06:31 -0300

    feat: ensure LoadParticipantsController throws if LoadParticipants throws

--- src/presentation/controllers/barbecue-participant/load-participants/load-participant-controller.ts
+++ src/presentation/controllers/barbecue-participant/load-participants/load-participant-controller.ts
@@ -1,12 +1,17 @@
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
 import { Controller } from '@/presentation/protocols/controller'
 import { LoadParticipants } from '@/domain/usecases/barbecue-participant/load-participants'
+import { serverError } from '@/presentation/helpers/http/http-helper'
 
 export class LoadParticipantsController implements Controller {
   constructor (private readonly loadParticipants: LoadParticipants) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadParticipants.load(httpRequest.params.barbecueId)
-    return Promise.resolve(null)
+    try {
+      await this.loadParticipants.load(httpRequest.params.barbecueId)
+      return Promise.resolve(null)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
