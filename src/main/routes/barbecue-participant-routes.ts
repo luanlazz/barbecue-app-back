e3f9ea966162925444370eba1cf5d35ccaf3a8cf
luan.lazzari <luanlazzari@hotmail.com>
2020-07-28 19:32:14 -0300

    feat: ensure load participants without access token return forbidden

--- src/main/routes/barbecue-participant-routes.ts
+++ src/main/routes/barbecue-participant-routes.ts
@@ -1,8 +1,10 @@
 import { makeSaveParticipantController } from '@/main/factories/controllers/barbecue-participant/save/save-participant-controller-factory'
+import { makeLoadParticipantsController } from '@/main/factories/controllers/barbecue-participant/load/load-participants-controller-factory'
 import { adaptRoute } from '@/main/adapters/express-route-adapter'
 import { auth } from '@/main/middlewares/auth/auth'
 import { Router } from 'express'
 
 export default (route: Router): void => {
   route.put('/barbecue/:barbecueId/participants/:participantId?', auth, adaptRoute(makeSaveParticipantController()))
+  route.get('/barbecue/:barbecueId/participants/', auth, adaptRoute(makeLoadParticipantsController()))
 }
