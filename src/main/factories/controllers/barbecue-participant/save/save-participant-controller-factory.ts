5374908d78f513a523110b5381cbd3c6081ee0b4
luan.lazzari <luanlazzari@hotmail.com>
2020-07-29 01:36:17 -0300

    feat: ensure SaveParticipantController call LoadBarbecueById with correct value

--- src/main/factories/controllers/barbecue-participant/save/save-participant-controller-factory.ts
+++ src/main/factories/controllers/barbecue-participant/save/save-participant-controller-factory.ts
@@ -1,11 +1,11 @@
 import { makeSaveParticipantValidation } from './save-participant-validation-factory'
 import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
 import { makeDbSaveParticipant } from '@/main/factories/usecases/barbecue-participant/save/save-participant'
-import { makeDbLoadParticipants } from '@/main/factories/usecases/barbecue-participant/load/load-participants'
 import { SaveParticipantController } from '@/presentation/controllers/barbecue-participant/save/save-participant-controller'
 import { Controller } from '@/presentation/protocols/controller'
+import { makeDbLoadBarbecueById } from '@/main/factories/usecases/barbecue/load-by-id/load-barbecue-by-id'
 
 export const makeSaveParticipantController = (): Controller => {
-  const controller = new SaveParticipantController(makeSaveParticipantValidation(), makeDbSaveParticipant(), makeDbLoadParticipants())
+  const controller = new SaveParticipantController(makeSaveParticipantValidation(), makeDbLoadBarbecueById(), makeDbSaveParticipant())
   return makeLogControllerDecorator(controller)
 }
