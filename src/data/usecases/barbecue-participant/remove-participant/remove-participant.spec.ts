ade9ed6f5045d57048d16bbb107698dd7abb0e05
luan.lazzari <luanlazzari@hotmail.com>
2020-07-28 21:45:00 -0300

    test: ensure DbRemoveParticipant throws if RemoveParticipantRepository throws

--- src/data/usecases/barbecue-participant/remove-participant/remove-participant.spec.ts
+++ src/data/usecases/barbecue-participant/remove-participant/remove-participant.spec.ts
@@ -1,6 +1,7 @@
 import { DbRemoveParticipant } from './remove-participant'
 import { mockRemoveParticipantRepository } from '@/data/test'
 import { RemoveParticipantRepository } from '@/data/protocols/db/barbecue-participant/db-remove-participant'
+import { throwError } from '@/domain/test'
 
 type SutTypes = {
   sut: DbRemoveParticipant
@@ -17,10 +18,17 @@ const makeSut = (): SutTypes => {
 }
 
 describe('RemoveParticipant use case', () => {
-  test('Should call LoadParticipantsByBqRepository with correct values', async () => {
+  test('Should call RemoveParticipantRepository with correct values', async () => {
     const { sut, removeParticipantRepositoryStub } = makeSut()
     const removeSpy = jest.spyOn(removeParticipantRepositoryStub, 'remove')
     await sut.remove('any_barbecue_id', 'any_participant_id')
     expect(removeSpy).toHaveBeenCalledWith('any_barbecue_id', 'any_participant_id')
+  })
+
+  test('Should throws if RemoveParticipantRepository throws', async () => {
+    const { sut, removeParticipantRepositoryStub } = makeSut()
+    jest.spyOn(removeParticipantRepositoryStub, 'remove').mockImplementation(throwError)
+    const participants = sut.remove('any_barbecue_id', 'any_participant_id')
+    await expect(participants).rejects.toThrow()
   })
 })
