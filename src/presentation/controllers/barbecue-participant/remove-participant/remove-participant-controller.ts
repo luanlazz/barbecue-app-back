6913b0ee623c2ceb47af786972ecb834d6d13c06
luan.lazzari <luanlazzari@hotmail.com>
2020-07-28 21:19:18 -0300

    feat: ensure RemoveParticipantController return participants on success

--- src/presentation/controllers/barbecue-participant/remove-participant/remove-participant-controller.ts
+++ src/presentation/controllers/barbecue-participant/remove-participant/remove-participant-controller.ts
@@ -1,15 +1,15 @@
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
 import { Controller } from '@/presentation/protocols/controller'
 import { RemoveParticipant } from '@/domain/usecases/barbecue-participant/remove-participant'
-import { serverError, noContent } from '@/presentation/helpers/http/http-helper'
+import { serverError, noContent, ok } from '@/presentation/helpers/http/http-helper'
 
 export class RemoveParticipantController implements Controller {
   constructor (private readonly removeParticipants: RemoveParticipant) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      await this.removeParticipants.remove(httpRequest.params.barbecueId, httpRequest.params.participantId)
-      return noContent()
+      const participants = await this.removeParticipants.remove(httpRequest.params.barbecueId, httpRequest.params.participantId)
+      return participants.length ? ok(participants) : noContent()
     } catch (error) {
       return serverError(error)
     }
