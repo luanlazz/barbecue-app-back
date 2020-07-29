7ae714542f01882af154184f0c463915c44fda94
luan.lazzari <luanlazzari@hotmail.com>
2020-07-28 21:13:05 -0300

    feat: ensure RemoveParticipantController throws if RemoveParticipant throws

--- src/presentation/controllers/barbecue-participant/remove-participant/remove-participant-controller.ts
+++ src/presentation/controllers/barbecue-participant/remove-participant/remove-participant-controller.ts
@@ -1,12 +1,17 @@
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
 import { Controller } from '@/presentation/protocols/controller'
 import { RemoveParticipant } from '@/domain/usecases/barbecue-participant/remove-participant'
+import { serverError } from '@/presentation/helpers/http/http-helper'
 
 export class RemoveParticipantController implements Controller {
   constructor (private readonly removeParticipants: RemoveParticipant) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.removeParticipants.remove(httpRequest.params.barbecueId, httpRequest.params.participantId)
-    return Promise.resolve(null)
+    try {
+      await this.removeParticipants.remove(httpRequest.params.barbecueId, httpRequest.params.participantId)
+      return Promise.resolve(null)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
