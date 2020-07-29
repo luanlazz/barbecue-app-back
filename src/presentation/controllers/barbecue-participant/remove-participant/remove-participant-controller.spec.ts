16c05a9fbf21058ceb5330baaabd241ac98d0243
luan.lazzari <luanlazzari@hotmail.com>
2020-07-28 21:13:40 -0300

    test: ensure RemoveParticipantController throws if RemoveParticipant throws

--- src/presentation/controllers/barbecue-participant/remove-participant/remove-participant-controller.spec.ts
+++ src/presentation/controllers/barbecue-participant/remove-participant/remove-participant-controller.spec.ts
@@ -2,6 +2,8 @@ import { RemoveParticipantController } from './remove-participant-controller'
 import { HttpRequest } from '@/presentation/protocols/http'
 import { mockRemoveParticipant } from '@/presentation/test/mock-participant'
 import { RemoveParticipant } from '@/domain/usecases/barbecue-participant/remove-participant'
+import { throwError } from '@/domain/test'
+import { serverError } from '@/presentation/helpers/http/http-helper'
 
 type SutTypes = {
   sut: RemoveParticipantController
@@ -27,8 +29,15 @@ const mockRequest = (): HttpRequest => ({
 describe('RemoveParticipant Controller', () => {
   test('should call RemoveParticipant with correct values', async () => {
     const { sut, removeParticipantStub } = makeSut()
-    const loadSpy = jest.spyOn(removeParticipantStub, 'remove')
+    const removeSpy = jest.spyOn(removeParticipantStub, 'remove')
     await sut.handle(mockRequest())
-    expect(loadSpy).toHaveBeenCalledWith(mockRequest().params.barbecueId, mockRequest().params.participantId)
+    expect(removeSpy).toHaveBeenCalledWith(mockRequest().params.barbecueId, mockRequest().params.participantId)
+  })
+
+  test('should return 500 if RemoveParticipant throws', async () => {
+    const { sut, removeParticipantStub } = makeSut()
+    jest.spyOn(removeParticipantStub, 'remove').mockImplementation(throwError)
+    const participants = await sut.handle(mockRequest())
+    expect(participants).toEqual(serverError(new Error()))
   })
 })
