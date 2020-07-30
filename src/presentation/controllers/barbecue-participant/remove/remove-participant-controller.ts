f294441b42413de1b759258f716457bfba52970a
luan.lazzari <luanlazzari@hotmail.com>
2020-07-30 01:55:48 -0300

    feat: ensure RemoveParticipantController call LoadBarbecueById with correct value

--- src/presentation/controllers/barbecue-participant/remove/remove-participant-controller.ts
+++ src/presentation/controllers/barbecue-participant/remove/remove-participant-controller.ts
@@ -1,14 +1,24 @@
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
 import { Controller } from '@/presentation/protocols/controller'
+import { serverError, noContent } from '@/presentation/helpers/http/http-helper'
 import { RemoveParticipant } from '@/domain/usecases/barbecue-participant/remove-participant'
-import { serverError } from '@/presentation/helpers/http/http-helper'
+import { LoadBarbecueById } from '@/domain/usecases/barbecue/load-barbecue-by-id'
 
 export class RemoveParticipantController implements Controller {
-  constructor (private readonly removeParticipants: RemoveParticipant) {}
+  constructor (
+    private readonly loadBarbecueById: LoadBarbecueById,
+    private readonly removeParticipants: RemoveParticipant
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      await this.removeParticipants.remove(httpRequest.params.barbecueId, httpRequest.params.participantId)
+      const { barbecueId, participantId } = httpRequest.params
+
+      await this.loadBarbecueById.loadById(barbecueId)
+
+      await this.removeParticipants.remove(barbecueId, participantId)
+
+      return noContent()
     } catch (error) {
       return serverError(error)
     }
