6bc7d8cbc2eb5df6947f3b09e1cd54a701abf044
luan.lazzari <luanlazzari@hotmail.com>
2020-07-30 01:56:20 -0300

    test: ensure RemoveParticipantController call LoadBarbecueById with correct value

--- src/presentation/controllers/barbecue-participant/remove/remove-participant-controller.spec.ts
+++ src/presentation/controllers/barbecue-participant/remove/remove-participant-controller.spec.ts
@@ -1,20 +1,25 @@
 import { RemoveParticipantController } from './remove-participant-controller'
 import { HttpRequest } from '@/presentation/protocols/http'
 import { mockRemoveParticipant } from '@/presentation/test/mock-participant'
+import { serverError, noContent } from '@/presentation/helpers/http/http-helper'
+import { mockLoadBarbecueById } from '@/presentation/test'
 import { RemoveParticipant } from '@/domain/usecases/barbecue-participant/remove-participant'
+import { LoadBarbecueById } from '@/domain/usecases/barbecue/load-barbecue-by-id'
 import { throwError } from '@/domain/test'
-import { serverError } from '@/presentation/helpers/http/http-helper'
 
 type SutTypes = {
   sut: RemoveParticipantController
+  loadBarbecueByIdStub: LoadBarbecueById
   removeParticipantStub: RemoveParticipant
 }
 
 const makeSut = (): SutTypes => {
+  const loadBarbecueByIdStub = mockLoadBarbecueById()
   const removeParticipantStub = mockRemoveParticipant()
-  const sut = new RemoveParticipantController(removeParticipantStub)
+  const sut = new RemoveParticipantController(loadBarbecueByIdStub, removeParticipantStub)
   return {
     sut,
+    loadBarbecueByIdStub,
     removeParticipantStub
   }
 }
@@ -27,6 +32,13 @@ const mockRequest = (): HttpRequest => ({
 })
 
 describe('RemoveParticipant Controller', () => {
+  test('should call LoadBarbecueById with correct barbecue id', async () => {
+    const { sut, loadBarbecueByIdStub } = makeSut()
+    const loadSpy = jest.spyOn(loadBarbecueByIdStub, 'loadById')
+    await sut.handle(mockRequest())
+    expect(loadSpy).toHaveBeenCalledWith(mockRequest().params.barbecueId)
+  })
+
   test('should call RemoveParticipant with correct values', async () => {
     const { sut, removeParticipantStub } = makeSut()
     const removeSpy = jest.spyOn(removeParticipantStub, 'remove')
@@ -37,7 +49,13 @@ describe('RemoveParticipant Controller', () => {
   test('should return 500 if RemoveParticipant throws', async () => {
     const { sut, removeParticipantStub } = makeSut()
     jest.spyOn(removeParticipantStub, 'remove').mockImplementation(throwError)
-    const participants = await sut.handle(mockRequest())
-    expect(participants).toEqual(serverError(new Error()))
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('should return 204 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(noContent())
   })
 })
