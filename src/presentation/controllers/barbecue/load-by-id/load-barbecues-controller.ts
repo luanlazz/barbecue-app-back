f9c7f6d593fc0d5cf2b77f7e98e8cee4c17a9a59
luan.lazzari <luanlazzari@hotmail.com>
2020-07-30 21:09:07 -0300

    feat: ensure LoadBarbecueByIdController throws if LoadBarbecueById throws

--- src/presentation/controllers/barbecue/load-by-id/load-barbecues-controller.ts
+++ src/presentation/controllers/barbecue/load-by-id/load-barbecues-controller.ts
@@ -1,12 +1,17 @@
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
 import { Controller } from '@/presentation/protocols/controller'
 import { LoadBarbecueById } from '@/domain/usecases/barbecue/load-barbecue-by-id'
+import { serverError } from '@/presentation/helpers/http/http-helper'
 
 export class LoadBarbecueByIdController implements Controller {
   constructor (private readonly loadBarbecueById: LoadBarbecueById) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadBarbecueById.loadById(httpRequest.params.barbecueId)
-    return null
+    try {
+      await this.loadBarbecueById.loadById(httpRequest.params.barbecueId)
+      return null
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
