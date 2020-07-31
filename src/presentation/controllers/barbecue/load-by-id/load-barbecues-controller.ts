257a26498ae26efcf3fff6bd0aadd17f8db6f7c8
luan.lazzari <luanlazzari@hotmail.com>
2020-07-30 21:16:41 -0300

    feat: ensure LoadBarbecueByIdController return 200 on success

--- src/presentation/controllers/barbecue/load-by-id/load-barbecues-controller.ts
+++ src/presentation/controllers/barbecue/load-by-id/load-barbecues-controller.ts
@@ -1,6 +1,6 @@
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
 import { Controller } from '@/presentation/protocols/controller'
-import { serverError, forbidden } from '@/presentation/helpers/http/http-helper'
+import { serverError, forbidden, ok } from '@/presentation/helpers/http/http-helper'
 import { InvalidParamError } from '@/presentation/errors'
 import { LoadBarbecueById } from '@/domain/usecases/barbecue/load-barbecue-by-id'
 
@@ -9,8 +9,10 @@ export class LoadBarbecueByIdController implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      await this.loadBarbecueById.loadById(httpRequest.params.barbecueId)
-      return forbidden(new InvalidParamError('barbecueId'))
+      const barbecue = await this.loadBarbecueById.loadById(httpRequest.params.barbecueId)
+      if (!barbecue) return forbidden(new InvalidParamError('barbecueId'))
+
+      return ok(barbecue)
     } catch (error) {
       return serverError(error)
     }
