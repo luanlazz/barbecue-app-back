db62d0299560255a35abb9625c3815eb04697317
luan.lazzari <luanlazzari@hotmail.com>
2020-08-15 16:50:16 -0300

    feat: ensure return false on invalid day if leap year

--- src/infra/validators/date-validator/date-validator-adapter.ts
+++ src/infra/validators/date-validator/date-validator-adapter.ts
@@ -1,7 +1,24 @@
 import { DateValidator } from '@/validation/protocols'
 
 export class DateValidatorAdapter implements DateValidator {
-  isValid (date: string): boolean {
-    return false
+  isValid (dateParam: string): boolean {
+    const dateRegex = /(\d{4})-([0]?[1-9]|[1][0-2])-([12][0-9]|3[01]|0?[1-9])T.*/
+
+    if (!dateRegex.test(dateParam)) {
+      return false
+    }
+
+    const split = dateParam.split('-')
+    const year = parseInt(split[0])
+    const month = parseInt(split[1])
+    const day = parseInt(split[2].split('T')[0])
+
+    const monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    if (year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0)) {
+      monthLength[1] = 29
+    }
+
+    return day > 0 && day <= monthLength[month - 1]
   }
 }
--- src/validation/protocols/date-validation.ts
+++ src/validation/protocols/date-validation.ts
@@ -1,3 +1,3 @@
 export interface DateValidator {
-  isValid (date: string): boolean
+  isValid(dateParam: string): boolean
 }
