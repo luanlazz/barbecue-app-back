8a997db3894f57544bb2c8ea3dbacee917370a9f
luan.lazzari <luanlazzari@hotmail.com>
2020-08-14 23:36:23 -0300

    feat: ensure LoadParticipantsController call LoadBarbecueById with correct values

--- src/presentation/controllers/barbecue-participant/load/load-participant-controller.ts
+++ src/presentation/controllers/barbecue-participant/load/load-participant-controller.ts
@@ -2,12 +2,17 @@ import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
 import { Controller } from '@/presentation/protocols/controller'
 import { LoadParticipants } from '@/domain/usecases/barbecue-participant/load-participants'
 import { serverError, noContent, ok } from '@/presentation/helpers/http/http-helper'
+import { LoadBarbecueById } from '@/domain/usecases/barbecue/load-barbecue-by-id'
 
 export class LoadParticipantsController implements Controller {
-  constructor (private readonly loadParticipants: LoadParticipants) {}
+  constructor (
+    private readonly loadBarbecueById: LoadBarbecueById,
+    private readonly loadParticipants: LoadParticipants
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      await this.loadBarbecueById.loadById(httpRequest.params.barbecueId)
       const participants = await this.loadParticipants.load(httpRequest.params.barbecueId)
       return participants.length ? ok(participants) : noContent()
     } catch (error) {
