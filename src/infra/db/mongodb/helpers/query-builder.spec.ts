2150e6532b91d9c220f7fb76b45881a860bb9bc0
luan.lazzari <luanlazzari@hotmail.com>
2020-08-15 19:05:22 -0300

    test: ensure QueryBuilder return a correct query

--- src/infra/db/mongodb/helpers/query-builder.spec.ts
+++ src/infra/db/mongodb/helpers/query-builder.spec.ts
@@ -3,37 +3,20 @@ import { QueryBuilder } from './query-builder'
 describe('Query Builder', () => {
   test('Should return valid match query', () => {
     const query = new QueryBuilder()
-      .match({ test: 'any_group' })
-      .build()
-
-    expect(query).toEqual([{
-      $match: {
-        test: 'any_group'
-      }
-    }])
-  })
-
-  test('Should return valid group query', () => {
-    const query = new QueryBuilder()
+      .match({ test: 'any_match' })
       .group({ test: 'any_group' })
+      .sort({ test: 'any_sort' })
+      .unwind({ test: 'any_unwind' })
+      .lookup({ test: 'any_lookup' })
+      .project({ test: 'any_project' })
       .build()
 
-    expect(query).toEqual([{
-      $group: {
-        test: 'any_group'
-      }
-    }])
-  })
-
-  test('Should return valid sort query', () => {
-    const query = new QueryBuilder()
-      .sort({ test: 'any_group' })
-      .build()
-
-    expect(query).toEqual([{
-      $sort: {
-        test: 'any_group'
-      }
-    }])
+    expect(query).toEqual([{ $match: { test: 'any_match' } },
+      { $group: { test: 'any_group' } },
+      { $sort: { test: 'any_sort' } },
+      { $unwind: { test: 'any_unwind' } },
+      { $lookup: { test: 'any_lookup' } },
+      { $project: { test: 'any_project' } }
+    ])
   })
 })
