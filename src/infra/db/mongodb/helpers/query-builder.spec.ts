76db6fa94dbe0efd667f833417fc8e1769e1119f
luan.lazzari <luanlazzari@hotmail.com>
2020-08-15 18:57:59 -0300

    test: ensure QueryBuilder return a valid sort query

--- src/infra/db/mongodb/helpers/query-builder.spec.ts
+++ src/infra/db/mongodb/helpers/query-builder.spec.ts
@@ -1,6 +1,18 @@
 import { QueryBuilder } from './query-builder'
 
 describe('Query Builder', () => {
+  test('Should return valid match query', () => {
+    const query = new QueryBuilder()
+      .match({ test: 'any_group' })
+      .build()
+
+    expect(query).toEqual([{
+      $match: {
+        test: 'any_group'
+      }
+    }])
+  })
+
   test('Should return valid group query', () => {
     const query = new QueryBuilder()
       .group({ test: 'any_group' })
@@ -13,13 +25,13 @@ describe('Query Builder', () => {
     }])
   })
 
-  test('Should return valid match query', () => {
+  test('Should return valid sort query', () => {
     const query = new QueryBuilder()
-      .match({ test: 'any_group' })
+      .sort({ test: 'any_group' })
       .build()
 
     expect(query).toEqual([{
-      $match: {
+      $sort: {
         test: 'any_group'
       }
     }])
