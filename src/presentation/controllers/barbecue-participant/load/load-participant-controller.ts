1b55bb069bd1420026c3748d7bd37fec162883c3
luan.lazzari <luanlazzari@hotmail.com>
2020-08-14 23:41:27 -0300

    feat: ensure LoadParticipantsController return forbidden if LoadBarbecueById return null

--- src/presentation/controllers/barbecue-participant/load/load-participant-controller.ts
+++ src/presentation/controllers/barbecue-participant/load/load-participant-controller.ts
@@ -1,8 +1,9 @@
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
 import { Controller } from '@/presentation/protocols/controller'
 import { LoadParticipants } from '@/domain/usecases/barbecue-participant/load-participants'
-import { serverError, noContent, ok } from '@/presentation/helpers/http/http-helper'
+import { serverError, noContent, ok, forbidden } from '@/presentation/helpers/http/http-helper'
 import { LoadBarbecueById } from '@/domain/usecases/barbecue/load-barbecue-by-id'
+import { InvalidParamError } from '@/presentation/errors'
 
 export class LoadParticipantsController implements Controller {
   constructor (
@@ -12,8 +13,11 @@ export class LoadParticipantsController implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      await this.loadBarbecueById.loadById(httpRequest.params.barbecueId)
+      const barbecue = await this.loadBarbecueById.loadById(httpRequest.params.barbecueId)
+      if (!barbecue) return forbidden(new InvalidParamError('barbecueId'))
+
       const participants = await this.loadParticipants.load(httpRequest.params.barbecueId)
+
       return participants.length ? ok(participants) : noContent()
     } catch (error) {
       return serverError(error)
