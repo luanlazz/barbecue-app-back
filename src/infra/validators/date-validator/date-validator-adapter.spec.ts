9534c2ba98099656da80195cf7b765aa9bf65bc3
luan.lazzari <luanlazzari@hotmail.com>
2020-08-15 16:50:51 -0300

    test: ensure return false on invalid day if leap year

--- src/infra/validators/date-validator/date-validator-adapter.spec.ts
+++ src/infra/validators/date-validator/date-validator-adapter.spec.ts
@@ -7,7 +7,27 @@ const makeSut = (): DateValidatorAdapter => {
 describe('DateValidatorAdapter adapter', () => {
   test('Should return false if format invalid', () => {
     const sut = makeSut()
-    const result = sut.isValid('2020/02/01')
+    const result = sut.isValid('2020/02/01T00:00:00')
     expect(result).toBeFalsy()
+  })
+
+  test('Should return false if date is invalid', () => {
+    const sut = makeSut()
+    const result = sut.isValid('2020-13-01T00:00:00')
+    expect(result).toBeFalsy()
+    const result2 = sut.isValid('2020-02-32T00:00:00')
+    expect(result2).toBeFalsy()
+  })
+
+  test('Should return false if day is invalid', () => {
+    const sut = makeSut()
+    const result = sut.isValid('2019-02-29T00:00:00')
+    expect(result).toBeFalsy()
+  })
+
+  test('Should return true if day is valid', () => {
+    const sut = makeSut()
+    const result = sut.isValid('2020-02-29T00:00:00')
+    expect(result).toBeTruthy()
   })
 })
