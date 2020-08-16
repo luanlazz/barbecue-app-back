f4a0fcf532985526cd7eb7967071758d5954816f
luan.lazzari <luanlazzari@hotmail.com>
2020-08-15 21:59:53 -0300

    test: ensure ValidationComposite return first error if more then one validation fails

--- src/validation/validators/validation-composite.spec.ts
+++ src/validation/validators/validation-composite.spec.ts
@@ -29,4 +29,12 @@ describe('Validation Composite', () => {
     const error = sut.validate({ [field]: faker.random.word() })
     expect(error).toEqual(validationSpies[1].error)
   })
+
+  test('Should return first error if more then one validation fails ', () => {
+    const { sut, validationSpies } = makeSut()
+    validationSpies[0].error = new Error()
+    validationSpies[1].error = new MissingParamError(field)
+    const error = sut.validate({ [field]: faker.random.word() })
+    expect(error).toEqual(validationSpies[0].error)
+  })
 })
