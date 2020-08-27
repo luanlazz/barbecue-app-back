6fa2c89918be2d46ccd6810a82545a5d4cbb2d23
Luan Lazzari <luanlazzari@hotmail.com>
2020-08-27 01:04:11 -0300

    test: ensure NumberValidation not return if validation success

--- src/validation/validators/number-validation/number-validation.spec.ts
+++ src/validation/validators/number-validation/number-validation.spec.ts
@@ -1,15 +1,34 @@
 import { NumberValidation } from './number-validation'
+import { NumberValidator } from '@/validation/protocols/number-validation'
+import { mockNumberValidator } from '@/validation/test'
 import { InvalidParamError } from '@/presentation/errors'
 import faker from 'faker'
 
-const makeSut = (): NumberValidation => {
-  return new NumberValidation('field')
+type SutTypes = {
+  numberValidator: NumberValidator
+  sut: NumberValidation
+}
+
+const makeSut = (): SutTypes => {
+  const numberValidator = mockNumberValidator()
+  const sut = new NumberValidation(numberValidator, 'field')
+  return {
+    numberValidator,
+    sut
+  }
 }
 
 describe('Number Validation', () => {
   test('Should return a InvalidParamError if validation fails', () => {
-    const sut = makeSut()
+    const { sut, numberValidator } = makeSut()
+    jest.spyOn(numberValidator, 'isValid').mockReturnValueOnce(false)
     const error = sut.validate({ value: faker.random.number({ min: -100, max: 0 }) })
     expect(error).toEqual(new InvalidParamError('field'))
+  })
+
+  test('Should not return if validation success', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ value: faker.random.number({ min: 0 }) })
+    expect(error).toBeFalsy()
   })
 })
