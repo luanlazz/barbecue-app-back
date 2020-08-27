881dcbafa7dba2ed75bd8fe76949193de2c17db2
Luan Lazzari <luanlazzari@hotmail.com>
2020-08-27 01:11:07 -0300

    test: ensure NumberValidation calls NumberValidator with correct value

--- src/validation/validators/number-validation/number-validation.spec.ts
+++ src/validation/validators/number-validation/number-validation.spec.ts
@@ -9,9 +9,9 @@ type SutTypes = {
   sut: NumberValidation
 }
 
-const makeSut = (): SutTypes => {
+const makeSut = (field = faker.database.column()): SutTypes => {
   const numberValidator = mockNumberValidator()
-  const sut = new NumberValidation(numberValidator, 'field')
+  const sut = new NumberValidation(numberValidator, field)
   return {
     numberValidator,
     sut
@@ -19,16 +19,26 @@ const makeSut = (): SutTypes => {
 }
 
 describe('Number Validation', () => {
+  test('Should calls NumberValidator with correct value', () => {
+    const field = faker.database.column()
+    const { sut, numberValidator } = makeSut(field)
+    const validationSpy = jest.spyOn(numberValidator, 'isValid')
+    const value = faker.random.number()
+    sut.validate({ [field]: value })
+    expect(validationSpy).toHaveBeenCalledWith(value)
+  })
+
   test('Should return a InvalidParamError if validation fails', () => {
-    const { sut, numberValidator } = makeSut()
+    const field = faker.database.column()
+    const { sut, numberValidator } = makeSut(field)
     jest.spyOn(numberValidator, 'isValid').mockReturnValueOnce(false)
-    const error = sut.validate({ value: faker.random.number({ min: -100, max: 0 }) })
-    expect(error).toEqual(new InvalidParamError('field'))
+    const error = sut.validate({ value: faker.random.number() })
+    expect(error).toEqual(new InvalidParamError(field))
   })
 
   test('Should not return if validation success', () => {
     const { sut } = makeSut()
-    const error = sut.validate({ value: faker.random.number({ min: 0 }) })
+    const error = sut.validate({ value: faker.random.number() })
     expect(error).toBeFalsy()
   })
 })
